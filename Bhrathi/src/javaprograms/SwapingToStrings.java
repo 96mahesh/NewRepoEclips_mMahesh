fca8a2efbb863c60f2b94d0b5701c8dcf52eff01
96Mahesh <96@mahesh>
2023-02-09 10:40:15 +0530

    one class modification

--- Bhrathi/src/javaprograms/SwapingToStrings.java
+++ Bhrathi/src/javaprograms/SwapingToStrings.java
@@ -4,12 +4,11 @@ public class SwapingToStrings {
 public static void main(String[] args) {
 	String a = "mahesh";
 	String b= "Babu";
-	
-	System.out.println("Before Swaping "+a+" "+b);
-	a=a+b;
-	b = a.substring(0,a.length()-b.length());
+	System.out.println("Before swaping "+a+" "+b);
+	a = a+b;
+	b = a.substring(0, a.length()-b.length());
 	a = a.substring(b.length());
-	System.out.println("After Swaping "+a+" "+b);
+    System.out.println("After swaping "+a+" "+b);
 
 
 }
