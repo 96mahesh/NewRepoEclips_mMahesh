6032a468c1283ede5b9077bd2dfaed9b922d7ddd
96Mahesh <96@mahesh>
2023-03-16 22:01:52 +0530

    Chang webtable class objects

--- Bhrathi/src/SeleniumPrograms/WebTableTest.java
+++ Bhrathi/src/SeleniumPrograms/WebTableTest.java
@@ -18,10 +18,18 @@ public class WebTableTest {
 	
 	By ColL =By.xpath("//table[@class='dataTable']/thead/tr/th");
 	List<WebElement> col = driver.findElements(ColL);
+	for (WebElement Ele : col) {
+		String data = Ele.getText();
+		System.out.println(data);
+	}
 	System.out.println("Columun Size = : " +col.size());
 	
 	By RowL =By.xpath("//table[@class='dataTable']/tbody/tr");
 	List<WebElement> Row = driver.findElements(RowL);
+	for (WebElement ele : Row) {
+		String actualdata = ele.getText();
+		System.out.println(actualdata);
+	}
 	System.out.println("RowSize Size = : " +Row.size());
 }
 }
