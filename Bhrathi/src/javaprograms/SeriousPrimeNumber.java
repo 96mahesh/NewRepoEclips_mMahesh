6de1ee1cb6ad33866438560b51867b6c8c337221
96Mahesh <96@mahesh>
2023-04-10 21:55:20 +0530

    modify class names

--- Bhrathi/src/javaprograms/SeriousPrimeNumber.java
+++ Bhrathi/src/javaprograms/SeriousPrimeNumber.java
@@ -4,32 +4,20 @@ public class SeriousPrimeNumber {
 
 	public static void main(String[] args) {
 		
-	    int lw =100;
-	    int up = 1000;
-	    int count = 0;
-	    for(int i=lw;i<=up;i++)
-	    {
-	    	int fact = 0;
-	    	for(int j=1;j<=i;j++)
-	    	{
-	    		
-	    		if(i%j==0)
-	    		{
-	    			fact ++;
-	    		}
-	    		
-	    	}
-	    	if(fact == 2)
-	    	{
-	    		count ++;
-	    		System.out.println(i);
-	    	}
-	    	
-	    }
-	    System.out.println(count);
-	    
-	}
-	
-	
+		int lw = 100,up = 1000;
+		int count = 0;
 		
-}
+		for(int i=lw;i<=up;i++) {
+			int fact = 0;
+			for(int j=1;j<=i;j++) {
+				if(i%j==0)
+					fact++;
+			}
+			if(fact==2) {
+				count++;
+				System.out.println(i);
+			}
+			 }
+		System.out.println(count);
+		}
+	}
